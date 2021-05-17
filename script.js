fcfe466cde4c750e5063ff80f019bbd4741d6d6b
shivam4490 <csg9152@gmail.com>
2021-05-17 20:24:54 +0530

    display income and expense

--- script.js
+++ script.js
@@ -7,10 +7,10 @@ const text = document.getElementById('text');
 const amount = document.getElementById('amount');
 
 // const dummyTransactions = [
-//   { id: 1, text: 'Flower', amount: -20 },
-//   { id: 2, text: 'Salary', amount: 300 },
-//   { id: 3, text: 'Book', amount: -10 },
-//   { id: 4, text: 'Camera', amount: 150 }
+//    { id: 1, text: 'Flower', amount: -20 },
+//    { id: 2, text: 'Salary', amount: 300 },
+//    { id: 3, text: 'Book', amount: -10 },
+//    { id: 4, text: 'Camera', amount: 150 },
 // ];
 
 // Add transactions to DOM list
@@ -32,6 +32,28 @@ function addTransactionDOM(transaction) {
     `;
 
    list.appendChild(item);
+}
+
+// Update the balance, income and expense
+function updateValues() {
+   const amounts = transactions.map((transaction) => transaction.amount);
+
+   const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+
+   const income = amounts
+      .filter((item) => item > 0)
+      .reduce((acc, item) => (acc += item), 0)
+      .toFixed(2);
+
+   const expense = (
+      amounts
+         .filter((item) => item < 0)
+         .reduce((acc, item) => (acc += item), 0) * -1
+   ).toFixed(2);
+
+   balance.innerText = `$${total}`;
+   money_plus.innerText = `$${income}`;
+   money_minus.innerText = `$${expense}`;
 }
 
 // Init app
